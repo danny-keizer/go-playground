fdb4fc2895891e8991407045f6066cedd7bc38e0
danny-keizer <danny.keizer@protonmail.com>
2026-09-18 11:49:34 +0200

    New project

--- shadow.go
+++ shadow.go
@@ -1,15 +1,88 @@
 package main
 
-import (
-	"fmt"
-)
-
-func add(x, y int) (int, error) {
-	return x + y, nil
+// This defines the database transaction struct.
+type DatabaseTransaction struct {
+	database databaseConnector
 }
 
+// This defines the database connector interface.
+type databaseConnector interface {
+	create() string
+	read() string
+	update() string
+	delete() string
+}
+
+// This agent speaks to postgresql.
+type postgresqlAgent struct{}
+
+func (p *postgresqlAgent) create() string {
+	return "Created something inside the postgresql database."
+}
+
+func (p *postgresqlAgent) read() string {
+	return "Read something inside the postgresql database."
+}
+
+func (p *postgresqlAgent) update() string {
+	return "Updated something inside the postgresql database."
+}
+
+func (p *postgresqlAgent) delete() string {
+	return "Deleted something inside the postgresql database."
+}
+
+// This agent speaks to mariadb.
+type mariadbAgent struct{}
+
+func (p *mariadbAgent) create() string {
+	return "Created something inside the mariadb database."
+}
+
+func (p *mariadbAgent) read() string {
+	return "Read something inside the mariadb database."
+}
+
+func (p *mariadbAgent) update() string {
+	return "Updated something inside the mariadb database."
+}
+
+func (p *mariadbAgent) delete() string {
+	return "Deleted something inside the mariadb database."
+}
+
+// Boilerplate.
+
+func NewDatabaseTransaction(connector databaseConnector) *DatabaseTransaction {
+	return &DatabaseTransaction{}
+}
+
+func dbCreate(transaction DatabaseTransaction) string {
+	return transaction.database.create()
+}
+
+func dbRead(transaction DatabaseTransaction) string {
+	return transaction.database.read()
+}
+
+func dbUpdate(transaction DatabaseTransaction) string {
+	return transaction.database.delete()
+}
+
+func dbDelete(transaction DatabaseTransaction) string {
+	return transaction.database.delete()
+}
+
+// Then main entry point.
 func main() {
-	var number int64 = 7
-	fmt.Println("Like your never departing shadow.", number)
-	fmt.Printf("Your final number is: %d", number)
+
+	var databaseType string = "postgresql"
+	_ = databaseType
+
+	myTransaction := NewDatabaseTransaction(&postgresqlAgent{})
+	dbCreate(*myTransaction)
+	dbRead(*myTransaction)
+	dbUpdate(*myTransaction)
+	dbDelete(*myTransaction)
+
 }
